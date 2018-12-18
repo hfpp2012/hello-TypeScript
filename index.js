14990483fcc85c853416f5298e8523c4a00d9416
hfpp2012 <hfpp2012@gmail.com>
2018-12-18 15:32:08 +0800

    05_var_let_const

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-var a;
-var b;
-var c;
-a = 111;
-b = true;
-c = "hello world";
+// if (true) {
+//     let i = 0;
+// }
+// let i = 0;
+// console.log(i);
+// for(let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+// console.log(i);
+// 定义类型和赋值
+var my_name = "rails365";
+console.log("my name is" + my_name);
+my_name = "1111";
+console.log("my name is " + my_name);
--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
-var a: number;
-var b: boolean;
-var c: string;
+// if (true) {
+//     let i = 0;
+// }
 
-a = 111;
-b = true;
-c = "hello world";
+// let i = 0;
+
+// console.log(i);
+
+// for(let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// console.log(i);
+// 定义类型和赋值
+let my_name: string = "rails365";
+console.log("my name is" + my_name);
+
+let my_name: string = "hfpp2012";
+console.log(`my name is ${my_name}`);
