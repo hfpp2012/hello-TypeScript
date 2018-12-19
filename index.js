8ba52be5882d85857b6c127fbcd2a43039583d77
hfpp2012 <hfpp2012@gmail.com>
2018-12-19 11:30:17 +0800

    09_function_return_value

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // function add(a: number, b: number) {
 // 	return a + b;
 // }
+// const add = (a: number, b: number): number => {
+// 	return a + b;
+// }
 var add = function (a, b) {
-    return a + b;
+    console.log(a + b);
 };
+// let sum = add(10, 20);
+// console.log(sum.toFixed(2));
+// console.log(sum.substr(0, 3));
 var sum = add(10, 20);
 console.log(sum);
--- index.ts
+++ index.ts
@@ -2,9 +2,17 @@
 // 	return a + b;
 // }
 
-const add = (a: number, b: number) => {
-	return a + b;
+// const add = (a: number, b: number): number => {
+// 	return a + b;
+// }
+
+const add = (a: number, b: number): void => {
+	console.log(a + b)
 }
+
+// let sum = add(10, 20);
+// console.log(sum.toFixed(2));
+// console.log(sum.substr(0, 3));
 
 let sum = add(10, 20);
 console.log(sum);
