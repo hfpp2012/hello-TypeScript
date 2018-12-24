63354ab8908e3004d5ca912e38793d892fbac2a2
hfpp2012 <hfpp2012@gmail.com>
2018-12-24 08:18:52 +0800

    10_function_default_arguments

--- index.js
+++ index.js
@@ -4,11 +4,27 @@
 // const add = (a: number, b: number): number => {
 // 	return a + b;
 // }
+// const add = (a: number, b: number = 10): void => {
+// 	console.log(a + b)
+// }
+// const add = (a: number, b?: number): void => {
+// 	if (b) {
+// 		console.log(a + b);
+// 	} else {
+// 		console.log(a);
+// 	}
+// }
 var add = function (a, b) {
-    console.log(a + b);
+    if (b === void 0) { b = 10; }
+    if (b) {
+        console.log(a + b);
+    }
+    else {
+        console.log(a);
+    }
 };
 // let sum = add(10, 20);
 // console.log(sum.toFixed(2));
 // console.log(sum.substr(0, 3));
-var sum = add(10, 20);
+var sum = add(20, 20);
 console.log(sum);
--- index.ts
+++ index.ts
@@ -6,13 +6,29 @@
 // 	return a + b;
 // }
 
-const add = (a: number, b: number): void => {
-	console.log(a + b)
+// const add = (a: number, b: number = 10): void => {
+// 	console.log(a + b)
+// }
+
+// const add = (a: number, b?: number): void => {
+// 	if (b) {
+// 		console.log(a + b);
+// 	} else {
+// 		console.log(a);
+// 	}
+// }
+
+const add = (a: number, b = 10): void => {
+	if (b) {
+		console.log(a + b);
+	} else {
+		console.log(a);
+	}
 }
 
 // let sum = add(10, 20);
 // console.log(sum.toFixed(2));
 // console.log(sum.substr(0, 3));
 
-let sum = add(10, 20);
+let sum = add(20, 20);
 console.log(sum);
