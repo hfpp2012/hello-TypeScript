b6104abb6ef1d900854d6497b2311305a7d72448
hfpp2012 <hfpp2012@gmail.com>
2019-01-04 09:22:30 +0800

    17_Member_visibility

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 // 模板
 var Person = /** @class */ (function () {
     function Person() {
@@ -18,34 +5,57 @@ var Person = /** @class */ (function () {
     Person.prototype.greet = function () {
         console.log('hi');
     };
+    Person.prototype.sayHi = function () {
+        console.log('private say hi');
+    };
+    Person.prototype.callSayHi = function () {
+        this.sayHi();
+        console.log('********');
+    };
+    // public
+    // get 方法
+    Person.prototype.getFirstName = function () {
+        console.log(this.firstName);
+    };
+    // public
+    // set 方法
+    Person.prototype.setFirstName = function (firstName) {
+        this.firstName = firstName;
+    };
     Person.prototype.othergreet = function () {
         this.greet();
         console.log('*****');
     };
     return Person;
 }());
+var aPerson = new Person();
+// 相当于 set 方法
+// aPerson.firstName = "rails365"
+// 相当于 get 方法
+// console.log(aPerson.firstName);
+aPerson.setFirstName("hfpp2012");
+aPerson.getFirstName();
+// aPerson.sayHi();
+aPerson.callSayHi();
 // 继承成了父类的数据和行为，就是属性和方法
 // 它的父类就是 Person
-var Programmer = /** @class */ (function (_super) {
-    __extends(Programmer, _super);
-    function Programmer() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    Programmer.prototype.greet = function () {
-        console.log('hello world');
-    };
-    // super 代表父类
-    Programmer.prototype.greetLikeNormalPeople = function () {
-        _super.prototype.greet.call(this);
-    };
-    return Programmer;
-}(Person));
+// class Programmer extends Person {
+// 	public greet() {
+// 		console.log('hello world');
+// 	}
+// 	// super 代表父类
+// 	public greetLikeNormalPeople() {
+// 		super.greet();
+// 	}
+// }
 // let aProgrammer: Person = new Programmer();
-var aProgrammer = new Person();
+// let aProgrammer: Programmer = new Person();
 // 调用方法时，先找自己本身对象的方法，如果没有，会找父类的
 // aProgrammer.greet();
 // aProgrammer.greetLikeNormalPeople();
-aProgrammer.othergreet();
+// aProgrammer.othergreet();
+// aProgrammer.firstName = "rails365";
+// console.log(aProgrammer.firstName);
 // 生成一个对象
 // let aPerson = new Person();
 // 设置 firstName 的内容
--- index.ts
+++ index.ts
@@ -1,40 +1,74 @@
 // 模板
 class Person {
 	// 定义了两个数据内容
-	firstName: string;
-	lastName: string;
+	private firstName: string;
+	private lastName: string;
 
-	greet() {
+	public greet() {
 		console.log('hi');
 	}
 
-	othergreet() {
+	private sayHi() {
+		console.log('private say hi');
+	}
+
+	public callSayHi() {
+		this.sayHi();
+		console.log('********');
+	}
+
+	// public
+	// get 方法
+	getFirstName() {
+		console.log(this.firstName);
+	}
+
+	// public
+	// set 方法
+	setFirstName(firstName) {
+		this.firstName = firstName;
+	}
+
+	public othergreet() {
 		this.greet();
 		console.log('*****');
 	}
 }
 
+let aPerson = new Person();
+// 相当于 set 方法
+// aPerson.firstName = "rails365"
+// 相当于 get 方法
+// console.log(aPerson.firstName);
+aPerson.setFirstName("hfpp2012");
+aPerson.getFirstName();
+
+// aPerson.sayHi();
+aPerson.callSayHi();
+
 // 继承成了父类的数据和行为，就是属性和方法
 // 它的父类就是 Person
-class Programmer extends Person {
-	greet() {
-		console.log('hello world');
-	}
+// class Programmer extends Person {
+// 	public greet() {
+// 		console.log('hello world');
+// 	}
 
-	// super 代表父类
-	greetLikeNormalPeople() {
-		super.greet();
-	}
-}
+// 	// super 代表父类
+// 	public greetLikeNormalPeople() {
+// 		super.greet();
+// 	}
+// }
 
-let aProgrammer: Person = new Programmer();
+// let aProgrammer: Person = new Programmer();
 // let aProgrammer: Programmer = new Person();
 // 调用方法时，先找自己本身对象的方法，如果没有，会找父类的
 // aProgrammer.greet();
 
 // aProgrammer.greetLikeNormalPeople();
 
-aProgrammer.othergreet();
+// aProgrammer.othergreet();
+// aProgrammer.firstName = "rails365";
+// console.log(aProgrammer.firstName);
 
 // 生成一个对象
 // let aPerson = new Person();
