64d122ee6c74b4ca066560e5f0466fd9adf47148
hfpp2012 <hfpp2012@gmail.com>
2019-01-15 10:36:02 +0800

    26_interface_method

--- index.js
+++ index.js
@@ -3,16 +3,16 @@
 // o 可以是一个接口类型，这个接口类型要有 name 属性
 // 定义 o 为接口类型，可以传入一个对象
 var sayName = function (o) {
-    console.log(o.name);
+    // console.log(o.name);
+    o.print(o.name);
 };
-var myname = function () {
-    name: "xxxx";
-};
-sayName(myname);
 // 对象
 var person = {
     age: 27,
-    name: "rails365"
+    name: "rails365",
+    print: function (name) {
+        console.log(name);
+    }
 };
 sayName(person);
 // const bottle = {
@@ -22,6 +22,9 @@ sayName(person);
 var Person = /** @class */ (function () {
     function Person() {
     }
+    Person.prototype.print = function (name) {
+        console.log(name);
+    };
     return Person;
 }());
 var aPerson = new Person();
--- index.ts
+++ index.ts
@@ -1,7 +1,13 @@
 // 接口
+// 传过来的参数必须包含接口定义的属性和方法
 interface Named {
 	// 属性
 	name: string
+
+	// 方法
+	// 没有方法体
+	// 具体的对象中实现方法体
+	print(name: string): void;
 }
 
 // 函数
@@ -9,15 +15,17 @@ interface Named {
 // o 可以是一个接口类型，这个接口类型要有 name 属性
 // 定义 o 为接口类型，可以传入一个对象
 const sayName = (o: Named) => {
-	console.log(o.name);
+	// console.log(o.name);
+	o.print(o.name);
 }
-
-sayName("...");
 
 // 对象
 const person = {
 	age: 27,
-	name: "rails365"
+	name: "rails365",
+	print: (name) => {
+		console.log(name)
+	}
 }
 
 sayName(person);
@@ -29,6 +37,10 @@ sayName(person);
 
 class Person {
 	name: string
+
+	print(name: string): void {
+		console.log(name);
+	}
 }
 
 let aPerson = new Person();
