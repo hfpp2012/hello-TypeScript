fdc6fe7d8f9712de65e00ad52354e174a83dc4e4
hfpp2012 <hfpp2012@gmail.com>
2019-01-16 09:34:46 +0800

    28_interface_class_type

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-// type Name = [string, number];
-// const user: User = {
-// 	name: "rails365",
-// 	age: 27
-// }
-var user = {
-    name: "rails365",
-    age: 27,
-    print: function () {
-        console.log('xxx');
+var sayName = function (o) {
+    o.greet();
+};
+// 实现接口，微信支付，支付宝支付
+// 类实现接口
+var Employee = /** @class */ (function () {
+    function Employee() {
     }
-};
-var other_user = {
-    name: "rails365",
-    age: 27,
-    email: "hfpp2012@gmail.com"
-};
+    Employee.prototype.greet = function () {
+        console.log('I am employee');
+    };
+    return Employee;
+}());
+var Customer = /** @class */ (function () {
+    function Customer() {
+    }
+    Customer.prototype.greet = function () {
+        console.log('I am customer');
+    };
+    return Customer;
+}());
+// let cu = new Customer();
+// cu.greet();
+// sayName(cu);
+// let em = new Employee();
+// em.greet();
+// sayName(em);
+var customer = new Customer();
+customer.greet();
+var employee = new Employee();
+employee.greet();
--- index.ts
+++ index.ts
@@ -1,44 +1,87 @@
-// type Name = [string, number];
-
-// 定义类型别名 type alias
-// 以后就可以用 Name 来代替 string 类型
-type Name = string;
-
-// let my_name: Name = ["rails365", 23];
-
-// let my_name:string = 'rails365';
-
-// 不能重复定义
-type User = {
+// 接口
+// 支付接口
+interface Person {
 	name: string;
-	age: number;
-	print(): void;
+
+	greet(): void;
 }
 
-// const user: User = {
-// 	name: "rails365",
-// 	age: 27
-// }
+const sayName = (o: Person) => {
+	o.greet();
+}
 
-const user: User = {
-	name: "rails365",
-	age: 27,
-	print: () => {
-		console.log('xxx');
+// 实现接口，微信支付，支付宝支付
+
+// 类实现接口
+class Employee implements Person {
+	name: string;
+
+	greet(): void {
+		console.log('I am employee');
 	}
 }
 
-interface IUser {
-	name: string;
-	age: number;
+class Customer implements Person {
+	public name: string;
+	public email: string;
+
+	greet(): void {
+		console.log('I am customer');
+	}
 }
 
-interface IUser {
-	email: string
+// let cu = new Customer();
+// cu.greet();
+
+// sayName(cu);
+
+// let em = new Employee();
+// em.greet();
+
+// sayName(em);
+
+let customer: Person = new Customer();
+customer.greet();
+
+let employee: Person = new Employee();
+employee.greet();
+
+// 支付接口
+
+interface Pay {
+	post(): void;
 }
 
-const other_user: IUser = {
-	name: "rails365",
-	age: 27,
-	email: "hfpp2012@gmail.com"
+// 可能会发送 http 请求
+// 真正支付的请求
+const do_pay = (pay: Pay) => {
+	// 有一些逻辑
+	pay.post()
 }
+
+// 微信支付
+class WePay implements Pay {
+	// 调微信支付的接口
+	post() {
+
+	}
+}
+
+// 支付宝支付
+class AliPay implements Pay {
+	// 调支付宝支付的接口
+	post() {
+
+	}
+}
+
+// 其他的支付接口
+
+let we_pay: Pay = new WePay();
+let ali_pay: Pay = new AliPay();
+
+// 微信支付
+do_pay(we_pay);
+
+// 支付宝支付
+do_pay(ali_pay);
