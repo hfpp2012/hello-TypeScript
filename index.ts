4895746866e601d1f4ce7f88c02fc0a417cdba62
hfpp2012 <hfpp2012@gmail.com>
2019-01-30 17:08:46 +0800

    34_interface_extends

--- index.ts
+++ index.ts
@@ -1,101 +1,35 @@
-// interface Person {
-// 	// 只读，不可修改
-// 	readonly first_name: string
-// 	last_name?: string
-
-// 	print(callback: PrintCallback): void
-
-// 	// 后面会讲
-// 	[propName: string]: any;
-// }
-
-// x 代表可以是任何类型，比如字符串 number 之类的
-let x: any = "hi there";
-// x 可以是任何类型，编译器可能不能明确知道 x 是哪种类型
-// <string> 表示把 x 断言成字符串类型，就是告诉编译器要把 x 当成字符串，这样
-// 就可以调用 substring 函数，因为字符串才有这个函数
-let s = (<string>x).substring(0, 3);
-console.log(typeof s);
-
-// 编译器可能不知道明确的类型，因为可以是两者之一
-function getLength(something: string | number): number {
-	if ((<string>something).length) {
-		return (<string>something).length;
-	} else {
-		return something.toString().length;
-	}
-	// return something.length
+interface Person {
+	name: string;
 }
 
-// 会报错
-// function toBoolean(something: string | number): boolean {
-// 	return <boolean>something;
-// }
+interface Employee {
+	age: number;
+}
 
-interface Person {
+// 继承另一个接口 Person
+// 会继承 Person 的属性的方法
+interface Programmer extends Person {
+	age: number;
+}
+
+let p: Programmer = {
+	age: 27,
+	name: "rails365"
+}
+
+// 类不能继承多个类，也就是说不能有多个父类
+// 但可以实现多个接口
+// 每个接口的属性和方法都要实现
+class P implements Person, Employee {
 	name: string;
 	age: number;
 }
 
-// 另一种类型断言的方式 as
-// 更好的明确类型和类型里面的结构
-let person = {} as Person;
-person.name = "rails365";
-person.age = 27;
-
-// 一种写法
-// react <div></div>
-let person1 = <Person>{
+let p1: P = {
 	name: "rails365",
-	age: 27
+	age: 25
 }
 
-let person: Person = {
-	name: "rails365",
-	age: 27
-}
+let p2: Person = p1;
 
-
-// interface PrintCallback {
-// 	// 可以简单理解为匿名函数
-// 	(success: boolean): void
-// }
-
-// let printCallback: PrintCallback;
-// printCallback = (suc: boolean): void => {
-// 	console.log("callback", suc);
-// }
-
-// let person: Person = {
-// 	first_name: "hfpp2012",
-// 	age: 27,
-// 	print: (callback: PrintCallback): void => {
-// 		console.log('hello');
-// 		callback(true);
-// 	}
-// }
-
-// person.print(printCallback);
-
-// person.first_name = "tony";
-
-// class Programmer implements Person {
-// 	first_name: string;
-// }
-
-// const programmer: Person = new Programmer();
-// programmer.first_name = "tony";
-
-// const sayName = (o: Person) => {
-//  console.log(o.first_name);
-// }
-
-// sayName(programmer);
-
-// sayName(person);
-
-// 类型断言
-// sayName({ first_name: "hfpp2012", last_name: "rails365", age: 27 } as Person)
-
-//
-// sayName({ first_name: "hfpp2012", lassst_name: "rails365", age: 27 })
+let p3: Employee = p1;
