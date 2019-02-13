69c4d85b88073fe3c235e981ef4f5060008e60a3
hfpp2012 <hfpp2012@gmail.com>
2019-02-13 23:26:39 +0800

    37_indexable_type_2

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ var s = { 'enabled': true, 'maximized': false };
 console.log(s);
 console.log(s["enabled"]);
 var s1 = [12, 34, 45, 1];
-console.log(s1);
-console.log(s1[0]);
+// console.log(s1.length);
+// console.log(s1.join('-'));
+// console.log(s1.push(23));
+// console.log(s1);
+// console.log(s1[0]);
 var s2 = [1, 2, 3];
-// let x = {a: 1, b: 2};
-// x['a'] // 1
-// x['b'] // 2
+var s3 = [true, false, false, false];
+console.log(s3);
+console.log(s3.length);
+console.log(s3.pop());
+var s4 = { mainScreen: 'sss' };
+var example = {
+    color: "red",
+    nest: {
+        '.subclass': {
+            color: "blue"
+        }
+    }
+};
+var c1 = {
+    color: "red"
+};
