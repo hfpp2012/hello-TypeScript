1c4d94b42e27d934a14050b6e48306f0d8c300f9
hfpp2012 <hfpp2012@gmail.com>
2019-02-15 17:30:16 +0800

    38_list

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-var s = { 'enabled': true, 'maximized': false };
-console.log(s);
-console.log(s["enabled"]);
-var s1 = [12, 34, 45, 1];
-// console.log(s1.length);
-// console.log(s1.join('-'));
-// console.log(s1.push(23));
-// console.log(s1);
-// console.log(s1[0]);
-var s2 = [1, 2, 3];
-var s3 = [true, false, false, false];
-console.log(s3);
-console.log(s3.length);
-console.log(s3.pop());
-var s4 = { mainScreen: 'sss' };
-var example = {
-    color: "red",
-    nest: {
-        '.subclass': {
-            color: "blue"
-        }
+var Todo1 = /** @class */ (function () {
+    function Todo1() {
     }
+    return Todo1;
+}());
+var todo = {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
 };
-var c1 = {
-    color: "red"
-};
+var todos = [
+    {
+        "userId": 1,
+        "id": 1,
+        "title": "delectus aut autem",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 2,
+        "title": "quis ut nam facilis et officia qui",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 3,
+        "title": "fugiat veniam minus",
+        "completed": false
+    }
+];
--- index.ts
+++ index.ts
@@ -1,65 +1,41 @@
-interface States {
-	[index: string]: boolean;
+interface Todo {
+	userId: number;
+	id: number;
+	title: string;
+	completed: boolean;
 }
 
-let s: States = { 'enabled': true, 'maximized': false }
-console.log(s);
-console.log(s["enabled"]);
-
-interface States1 {
-	// An index signature parameter type must be 'string' or 'number'.
-	// 这种方式定义的数组没有 push length join 等属性或方法
-	[index: number]: number;
+class Todo1 {
+	userId: number;
+	id: number;
+	title: string;
+	completed: boolean;
 }
 
-let s1: States1 = [12, 34, 45, 1];
-// console.log(s1.length);
-// console.log(s1.join('-'));
-// console.log(s1.push(23));
-// console.log(s1);
-// console.log(s1[0]);
-
-let s2: number[] = [1, 2, 3];
-// console.log(s2.length);
-// console.log(s2.join('-'));
-// s2.push(23);
-
-// let x = {a: 1, b: 2};
-// x['a'] // 1
-// x['b'] // 2
-
-interface States2 {
-	[index: number]: boolean;
-	length: number;
-	pop(): boolean;
+let todo: Todo = {
+	"userId": 1,
+	"id": 1,
+	"title": "delectus aut autem",
+	"completed": false
 }
 
-let s3: States2 = [true, false, false, false];
-console.log(s3);
-console.log(s3.length);
-console.log(s3.pop());
-
-interface States3 {
-	[state: string]: any;
-	mainScreen: string;
-}
-
-let s4: States3 = { mainScreen: 'sss' };
-// s4['enabled'] = false;
-// s4['maximized'] = true;
-
-interface NestedCss {
-	color?: string;
-	nest?: {
-		[selector: string]: NestedCss
+let todos: Todo1[] = [
+	{
+		"userId": 1,
+		"id": 1,
+		"title": "delectus aut autem",
+		"completed": false
+	},
+	{
+		"userId": 1,
+		"id": 2,
+		"title": "quis ut nam facilis et officia qui",
+		"completed": false
+	},
+	{
+		"userId": 1,
+		"id": 3,
+		"title": "fugiat veniam minus",
+		"completed": false
 	}
-}
-
-let example: NestedCss = {
-	color: "red",
-	nest: {
-		'.subclass': {
-			color: "blue"
-		}
-	}
-}
+]
