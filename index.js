f7ce20ba4e13c49c61740f14af02216d2293496d
hfpp2012 <hfpp2012@gmail.com>
2019-02-17 12:22:32 +0800

    41_Accessors

--- index.js
+++ index.js
@@ -6,12 +6,40 @@
 // 		this._age = age;
 // 	}
 // }
+// class Person {
+// 	constructor(private _name: string, private _age: number) {
+// 	}
+// }
 var Person = /** @class */ (function () {
-    function Person(_name, _age) {
-        this._name = _name;
-        this._age = _age;
+    function Person(name, age) {
+        this._name = name;
+        this._age = age;
     }
+    // 读取
+    Person.prototype.getName = function () {
+        return this._name;
+    };
+    // 设置
+    Person.prototype.setName = function (name) {
+        this._name = name;
+    };
+    Object.defineProperty(Person.prototype, "name", {
+        get: function () {
+            return this._name;
+        },
+        set: function (name) {
+            this._name = name;
+        },
+        enumerable: true,
+        configurable: true
+    });
     return Person;
 }());
 var p = new Person("hfpp2012", 29);
+// 输出 name
+console.log(p.name);
+// console.log(p.getName());
+// 修改 name
+p.name = 'rails365';
+// p.setName("rails365");
 console.log(p);
