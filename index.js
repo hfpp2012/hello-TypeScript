47b97b4c74c261f2ee5ecdfeeac393d31299818d
hfpp2012 <hfpp2012@gmail.com>
2019-02-17 10:06:37 +0800

    40_Parameter_Properties

--- index.js
+++ index.js
@@ -1,42 +1,17 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-// 不能实例化
+// class Person {
+// 	private _name: string;
+// 	private _age: number;
+// 	constructor(name: string, age: number) {
+// 		this._name = name;
+// 		this._age = age;
+// 	}
+// }
 var Person = /** @class */ (function () {
-    function Person() {
+    function Person(_name, _age) {
+        this._name = _name;
+        this._age = _age;
     }
-    // constructor(name: string) {
-    // 	this.name = name;
-    // }
-    Person.prototype.display = function () {
-        console.log(this.name);
-    };
     return Person;
 }());
-var Employee = /** @class */ (function (_super) {
-    __extends(Employee, _super);
-    function Employee(name, code) {
-        var _this = _super.call(this) || this;
-        _this.name = name;
-        _this.empCode = code;
-        return _this;
-    }
-    // 必须实现抽象的方法
-    Employee.prototype.find = function (name) {
-        return new Employee(name, 1);
-    };
-    return Employee;
-}(Person));
-var p = new Employee('hfpp2012', 1111);
-var p1 = p.find("rails365");
-console.log(p1);
+var p = new Person("hfpp2012", 29);
+console.log(p);
--- index.ts
+++ index.ts
@@ -1,36 +1,18 @@
-// 不能实例化
-abstract class Person {
-	abstract name: string;
+// class Person {
+// 	private _name: string;
+// 	private _age: number;
 
-	// constructor(name: string) {
-	// 	this.name = name;
-	// }
+// 	constructor(name: string, age: number) {
+// 		this._name = name;
+// 		this._age = age;
+// 	}
+// }
 
-	display(): void {
-		console.log(this.name);
-	}
-
-	// 抽象的方法
-	// 没有方法体
-	abstract find(string): Person;
-}
-
-class Employee extends Person {
-	name: string;
-	empCode: number;
-
-	constructor(name: string, code: number) {
-		super(); // 必须调用 super
-		this.name = name;
-		this.empCode = code;
-	}
-
-	// 必须实现抽象的方法
-	find(name: string): Person {
-		return new Employee(name, 1);
+class Person {
+	constructor(private _name: string, private _age: number) {
+		
 	}
 }
 
-let p: Person = new Employee('hfpp2012', 1111);
-let p1: Person = p.find("rails365");
-console.log(p1);
+let p: Person = new Person("hfpp2012", 29);
+console.log(p);
