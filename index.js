2147945dada60fa6dc2a59adeaacd70961a99e9d
hfpp2012 <hfpp2012@gmail.com>
2019-02-21 10:30:27 +0800

    45_function_overloading_2

--- index.js
+++ index.js
@@ -7,7 +7,33 @@ function sum(x, y, z) {
         return x + y + z;
     }
 }
-var n = sum(1, 2, 3);
+function divide(x, y) {
+    if (typeof x === 'number') {
+        return x / y;
+    }
+    else if (typeof x === 'string') {
+        return [x.substring(0, y), x.substring(y)];
+    }
+}
+var n = divide(6, 2);
 console.log(n);
-n = sum(1, 2);
-console.log(n);
+var s = divide("football", 3);
+console.log(s);
+var Util = /** @class */ (function () {
+    function Util() {
+    }
+    Util.divide = function (x, y) {
+        if (typeof x === 'number') {
+            return x / y;
+        }
+        else if (typeof x === 'string') {
+            return [x.substring(0, y), x.substring(y)];
+        }
+    };
+    return Util;
+}());
+// let a: Util = new Util();
+// console.log(a.divide(6, 2));
+// console.log(a.divide('hello world', 4));
+var c = Util.divide(6, 2);
+console.log(c);
