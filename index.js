213a57caa2acddb1afdc39ae9b3706e3fe4c914c
hfpp2012 <hfpp2012@gmail.com>
2019-03-06 10:45:43 +0800

    55_generics_classes

--- index.js
+++ index.js
@@ -1,55 +1,52 @@
-// function getArray(items: any[]): any[] {
-// 	return new Array().concat(items);
-// }
-// let myNumArray = getArray([100, 200, 300]);
-// let myStrArray = getArray(["hello", "world"]);
-// console.log(myNumArray);
-// console.log(myStrArray);
-// myNumArray.push(400);
-// myStrArray.push("rails365");
-// console.log(myNumArray);
-// console.log(myStrArray);
-// myNumArray.push("hfpp2012");
-// myStrArray.push(500);
-// console.log(myNumArray);
-// console.log(myStrArray);
-// [ 100, 200, 300, 400, 'hfpp2012' ]
-// [ 'hello', 'world', 'rails365', 500 ]
-// 可以用很多类型来代替 T
-// T[] 代表数组，每个元素都是 T 类型
-function getArray(items) {
-    return new Array().concat(items);
-}
-// function getArray(items: number[]): number[] {
-// 	return new Array().concat(items);
-// }
-// 调用，可以传入很多类型，代码复用
-var myNumArray = getArray([100, 200, 300]);
-var myStrArray = getArray(['hello', 'world']);
-// 当不传入 T 时，会推荐出类型
-// 不推荐这种写法，规范的作法是要指定类型
-var myBooleanArray = getArray([true, false]);
-myNumArray.push(400);
-myStrArray.push('rails365');
-// myBooleanArray.push('hfpp2012');
-// function getArray1<T>(items: T): T {
-// 	console.log(items.length);
-// 	return T;
-// }
-function getArray1(items) {
-    console.log(items.length);
-    return items;
-}
-// myNumArray.push('hfpp2012');
-// myStrArray.push(500);
-// 限制为所有的元素是纯数字或字符串
-// 
-// function getArray(items: number[]): number[] {
-// 	return new Array().concat(items);
-// }
-// function getArray(items: string[]): string[] {
-// 	return new Array().concat(items);
-// }
-// function getArray(items: boolean[]): boolean[] {
-// 	return new Array().concat(items);
-// }
+var List = /** @class */ (function () {
+    function List(elements) {
+        this.data = elements;
+    }
+    List.prototype.add = function (t) {
+        this.data.push(t);
+    };
+    List.prototype.remove = function (t) {
+        var index = this.data.indexOf(t);
+        if (index > -1) {
+            this.data.splice(index, 1);
+        }
+    };
+    List.prototype.asArray = function () {
+        return this.data;
+    };
+    return List;
+}());
+var numbers = new List([1, 2, 3, 4]);
+numbers.add(5);
+numbers.remove(3);
+var numArray = numbers.asArray();
+console.log(numArray);
+var fruits = new List(['apple', 'banana', 'orange']);
+fruits.add('mango');
+fruits.remove('apple');
+var fruitArray = fruits.asArray();
+console.log(fruitArray);
+var Pair = /** @class */ (function () {
+    function Pair(first, second) {
+        this._first = first;
+        this._second = second;
+    }
+    Object.defineProperty(Pair.prototype, "first", {
+        get: function () {
+            return this._first;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Pair.prototype, "second", {
+        get: function () {
+            return this._second;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Pair;
+}());
+var pair = new Pair(true, "111");
+console.log(pair.first);
+console.log(pair.second);
--- index.ts
+++ index.ts
@@ -1,74 +1,56 @@
-// function getArray(items: any[]): any[] {
-// 	return new Array().concat(items);
-// }
+class List<T> {
+	private data: T[];
 
-// let myNumArray = getArray([100, 200, 300]);
-// let myStrArray = getArray(["hello", "world"]);
+	constructor(elements: T[]) {
+		this.data = elements;
+	}
 
-// console.log(myNumArray);
-// console.log(myStrArray);
+	add(t: T) {
+		this.data.push(t);
+	}
 
-// myNumArray.push(400);
-// myStrArray.push("rails365");
+	remove(t: T) {
+		let index = this.data.indexOf(t);
+		if (index > -1) {
+			this.data.splice(index, 1);
+		}
+	}
 
-// console.log(myNumArray);
-// console.log(myStrArray);
-
-// myNumArray.push("hfpp2012");
-// myStrArray.push(500);
-
-// console.log(myNumArray);
-// console.log(myStrArray);
-
-// [ 100, 200, 300, 400, 'hfpp2012' ]
-// [ 'hello', 'world', 'rails365', 500 ]
-
-// 可以用很多类型来代替 T
-// T[] 代表数组，每个元素都是 T 类型
-function getArray<T>(items: T[]): T[] {
-	return new Array<T>().concat(items);
+	asArray(): T[] {
+		return this.data;
+	}
 }
 
-// function getArray(items: number[]): number[] {
-// 	return new Array().concat(items);
-// }
+let numbers = new List<number>([1, 2, 3, 4]);
+numbers.add(5);
+numbers.remove(3);
+let numArray = numbers.asArray();
+console.log(numArray);
 
-// 调用，可以传入很多类型，代码复用
-let myNumArray = getArray<number>([100, 200, 300]);
-let myStrArray = getArray<string>(['hello', 'world']);
+let fruits = new List<string>(['apple', 'banana', 'orange']);
+fruits.add('mango');
+fruits.remove('apple');
+let fruitArray = fruits.asArray();
+console.log(fruitArray);
 
-// 当不传入 T 时，会推荐出类型
-// 不推荐这种写法，规范的作法是要指定类型
-let myBooleanArray = getArray([true, false]);
+class Pair<F, S> {
+	private _first: F;
+	private _second: S;
 
-myNumArray.push(400);
-myStrArray.push('rails365');
-// myBooleanArray.push('hfpp2012');
+	constructor(first: F, second: S) {
+		this._first = first;
+		this._second = second;
+	}
 
-// function getArray1<T>(items: T): T {
-// 	console.log(items.length);
-// 	return T;
-// }
+	get first(): F {
+		return this._first;
+	}
 
-function getArray1<T>(items: T[]): T[] {
-	console.log(items.length);
-	return items;
+	get second(): S {
+		return this._second;
+	}
 }
 
-// myNumArray.push('hfpp2012');
-// myStrArray.push(500);
-
-// 限制为所有的元素是纯数字或字符串
-
-// 
-// function getArray(items: number[]): number[] {
-// 	return new Array().concat(items);
-// }
-
-// function getArray(items: string[]): string[] {
-// 	return new Array().concat(items);
-// }
-
-// function getArray(items: boolean[]): boolean[] {
-// 	return new Array().concat(items);
-// }
+let pair = new Pair<boolean, string>(true, "111");
+console.log(pair.first);
+console.log(pair.second);
