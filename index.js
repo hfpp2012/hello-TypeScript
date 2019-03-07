d11633fd8f98094b0c12af7488e338d2a570ed04
hfpp2012 <hfpp2012@gmail.com>
2019-03-07 21:28:07 +0800

    59_generics_interface_oop

--- index.js
+++ index.js
@@ -1,8 +1,57 @@
-// interface States<R> {
-// 	[state: string]: R
-// }
-var s = {
-    'enabled': { first: 1, second: true }, 'maximized': { first: 2, second: false }
-};
-console.log(s);
-console.log(s['enabled']);
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var List = /** @class */ (function () {
+    function List(elements) {
+        this.data = [];
+        this.data = elements;
+    }
+    List.prototype.add = function (t) {
+        this.data.push(t);
+    };
+    List.prototype.remove = function (t) {
+        var index = this.data.indexOf(t);
+        if (index > -1) {
+            this.data.splice(index, 1);
+        }
+    };
+    List.prototype.asArray = function () {
+        return this.data;
+    };
+    return List;
+}());
+var numbers = new List([1, 2, 3]);
+numbers.add(4);
+numbers.remove(2);
+console.log(numbers);
+// let strings: Collection<number> = new List<string>(["1, 2, 3", "2"]);
+var BookList = /** @class */ (function (_super) {
+    __extends(BookList, _super);
+    function BookList() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return BookList;
+}(List));
+var bookList = new BookList([true, false]);
+console.log(bookList);
+var bookList1 = new BookList([1, 2]);
+console.log(bookList1);
+var MovieList = /** @class */ (function (_super) {
+    __extends(MovieList, _super);
+    function MovieList() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return MovieList;
+}(List));
+var movieList = new MovieList([true, false]);
+console.log(movieList);
--- index.ts
+++ index.ts
@@ -1,23 +1,54 @@
-// interface States<R> {
-// 	[state: string]: R
-// }
-
-// let s: States<boolean> = { 'enabled': true, 'maximized': false };
-// console.log(s);
-// console.log(s['maximized']);
-
-interface Pair<F, S> {
-	first: F;
-	second: S;
+interface Collection<T> {
+	add(t: T): void;
+	remove(t: T): void;
+	asArray(): T[];
 }
 
-interface States<F, S> {
-	[state: string]: Pair<F, S>
+interface Collection1<T> extends Collection<T> {
+	getElementAt(index: number): T;
 }
 
-let s: States<number, boolean> = {
-	'enabled': { first: 1, second: true }, 'maximized': { first: 2, second: false }
-};
+class List<T> implements Collection<T> {
+	private data: T[] = [];
 
-console.log(s);
-console.log(s['enabled']);
+	constructor(elements: T[]) {
+		this.data = elements;
+	}
+
+	add(t: T): void {
+		this.data.push(t);
+	}
+
+	remove(t: T): void {
+		let index = this.data.indexOf(t);
+		if (index > -1) {
+			this.data.splice(index, 1);
+		}
+	}
+
+	asArray(): T[] {
+		return this.data;
+	}
+}
+
+let numbers: Collection<number> = new List<number>([1, 2, 3]);
+numbers.add(4);
+numbers.remove(2);
+console.log(numbers);
+
+// let strings: Collection<number> = new List<string>(["1, 2, 3", "2"]);
+
+class BookList<T> extends List<T> {
+
+}
+
+let bookList: BookList<boolean> = new BookList<boolean>([true, false])
+console.log(bookList);
+
+let bookList1: BookList<number> = new BookList<number>([1, 2]);
+console.log(bookList1);
+
+class MovieList extends List<boolean> {}
+
+let movieList: MovieList = new MovieList([true, false]);
+console.log(movieList);
