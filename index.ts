3bfdf3a8a23375fe406785eb70378edff409da04
hfpp2012 <hfpp2012@gmail.com>
2019-03-12 12:30:27 +0800

    63_generic_constraints_2

--- index.ts
+++ index.ts
@@ -25,6 +25,22 @@ class Test2 {
   }
 }
 
+function createInstance3<R, T extends { new(...constructorArgs: any[]): R }>(constructor: T, ...args: any[]): R {
+	return new constructor(args);
+}
+
+class Test3 {
+	private x: number;
+
+	constructor(x: number) {
+		this.x = x;
+	}
+
+}
+
+let test3: Test3 = createInstance3(Test3, 6);
+console.log(test3);
+
 // new Test2(3);
 
 let test2: Test2 = createInstance2(Test2, 3, 5);
